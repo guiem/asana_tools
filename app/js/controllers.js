467f0975bb6211e05e1d6176d6371dbbaf96a02c
guiem <g@guiem.info>
2014-02-05 08:19:48 +0100

    some changes in controller structure

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,7 @@
 var asanaToolsApp = angular.module('asanaToolsApp', []);
 var asanaBaseUrl = 'https://app.asana.com/api/1.0';
 
-function AuthCtrl($scope,$rootScope,$http) {
+function MainCtrl($scope,$rootScope,$http) {
     $scope.saveAuth = function() {
         $rootScope.auth = 'Basic ' + window.btoa($scope.apiKey+':');
         $http.defaults.headers.common['Authorization'] = $rootScope.auth;
@@ -13,17 +13,12 @@ function AuthCtrl($scope,$rootScope,$http) {
         $scope.workspaces = data;
         });
     }
-}
-
-function ListTasksCtrl($scope,$rootScope,$http) {
+    
     $scope.listTasks = function() {
-        window.alert('hey hey');
-        //$rootScope.auth = 'Basic ' + window.btoa($scope.apiKey+':');
-        //$http.defaults.headers.common['Authorization'] = $rootScope.auth;
-        $http.get(asanaBaseUrl+'/workspaces').success(function(data) {
+        $http.defaults.headers.common['Authorization'] = $rootScope.auth;
+        $http.get(asanaBaseUrl+'/workspaces/'+$scope.wsId+'/tasks').success(function(data) {
                                                       $scope.tasks = data;
                                                       });
-        window.alert($scope.tasks);
     }
 }
 
