b8afde933753b25d7a33869d1301d75762da5cfb
guiem <g@guiem.info>
2014-02-06 07:53:31 +0100

    listing tasks (not pretty view) given a workspace and a project

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,9 +14,16 @@ function MainCtrl($scope,$rootScope,$http) {
         });
     }
     
-    $scope.listTasks = function() {
-        $http.defaults.headers.common['Authorization'] = $rootScope.auth;
-        $http.get(asanaBaseUrl+'/workspaces/'+$scope.wsId+'/tasks').success(function(data) {
+    $scope.getProjects = function(wsId,wsName) {
+        $scope.workspace = wsName;
+        $http.get(asanaBaseUrl+'/workspaces/'+wsId+'/projects').success(function(data) {
+                                                                     $scope.projects = data;
+                                                                     });
+    }
+    
+    $scope.listTasks = function(pId,pName) {
+        $scope.project = pName;
+        $http.get(asanaBaseUrl+'/projects/'+pId+'/tasks').success(function(data) {
                                                       $scope.tasks = data;
                                                       });
     }
